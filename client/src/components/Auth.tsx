a1d062703710deebd3e51df8529fc4b6da7686ef
Wenceslas-Angelo <wenceslasangelo@gmail.com>
2024-03-15 14:31:12 +0300

    react hook form

--- client/src/components/Auth.tsx
+++ client/src/components/Auth.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import smmcLogo from "../assets/SMMC-Logo.png";
 import loginImg from "../assets/login.png";
 import registerImg from "../assets/register.png";
+import { AuthType } from "../types";
 import Button from "./Button";
 import Input from "./Input";
 
@@ -12,10 +14,20 @@ type Props = {
 };
 
 const Auth = ({ variant, title }: Props) => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<AuthType>();
+
+  const onSubmit: SubmitHandler<AuthType> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="grid min-h-screen grid-cols-1 md:grid-cols-2 bg-primary-200/50">
       <div
-        className={`flex items-center justify-center bg-gray-100 border-gray-400 ${
+        className={`flex items-center justify-center bg-gray-200 border-gray-400 ${
           variant === "login" ? "order-1 border-l-2" : "order-0 border-r-2"
         }`}
       >
@@ -30,15 +42,42 @@ const Auth = ({ variant, title }: Props) => {
             <img src={smmcLogo} alt="Logo SMMC" className="w-52" />
           </div>
           <h1 className="text-3xl capitalize text-gray">{title}</h1>
-          <form className="pt-5 pb-5 space-y-4">
+          <form
+            className="pt-5 pb-5 space-y-4"
+            onSubmit={handleSubmit(onSubmit)}
+          >
             {variant === "register" ? (
               <>
-                <Input type="text" placeholder="Nom" />
-                <Input type="text" placeholder="Prenom" />
+                <Input
+                  type="text"
+                  register={register}
+                  id="name"
+                  placeholder="Nom"
+                  errors={errors}
+                />
+                <Input
+                  type="text"
+                  register={register}
+                  id="firstName"
+                  placeholder="Prenom"
+                  errors={errors}
+                />
               </>
             ) : null}
-            <Input type="number" placeholder="Matricule" />
-            <Input type="password" placeholder="Mot de passe" />
+            <Input
+              type="number"
+              register={register}
+              id="matricule"
+              placeholder="Matricule"
+              errors={errors}
+            />
+            <Input
+              type="password"
+              register={register}
+              id="password"
+              placeholder="Mot de passe"
+              errors={errors}
+            />
             <Button type="submit">{title}</Button>
           </form>
           {variant === "login" ? (
--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,24 +1,44 @@
 import React from "react";
-import { UseFormRegister } from "react-hook-form";
+import { FieldErrors, UseFormRegister } from "react-hook-form";
 import { AuthType } from "../types";
 
 type Props = {
   type: "number" | "password" | "text";
   placeholder: string;
   isLoading?: boolean;
-  register?: UseFormRegister<AuthType>;
+  register: UseFormRegister<AuthType>;
+  errors: FieldErrors<AuthType>;
+  required?: boolean;
+  id: "name" | "firstName" | "matricule" | "password";
 };
 
-const Input = ({ type, placeholder, isLoading = false }: Props) => {
+const Input = ({
+  type,
+  placeholder,
+  isLoading = false,
+  register,
+  errors,
+  required = true,
+  id,
+}: Props) => {
   return (
     <div className="flex items-center">
       <input
         type={type}
         placeholder={placeholder}
-        className="w-full px-4 py-3 text-lg font-light placeholder-gray-600 bg-white border border-gray-400 rounded focus:outline-none focus:ring-1 focus:ring-primary"
-        name="email"
+        className={`w-full px-4 py-3 text-lg font-light bg-white border
+         ${
+           errors[id]
+             ? "border-rose-400 focus:ring-rose-600 placeholder-rose-600"
+             : "border-gray-400 focus:ring-green-600 placeholder-gray-600"
+         } rounded focus:outline-none focus:ring-1`}
+        id={id}
         disabled={isLoading}
         autoComplete="off"
+        data-testid="input"
+        {...register(id, {
+          required,
+        })}
       />
     </div>
   );
