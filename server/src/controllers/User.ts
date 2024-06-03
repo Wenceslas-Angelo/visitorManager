098853f560eae1e807aaf0962225d9ab1a5958d3
Wenceslas-Angelo <wenceslasangelo@gmail.com>
2024-06-03 15:16:56 +0300

    refactore User controller

--- server/src/controllers/User.ts
+++ server/src/controllers/User.ts
@@ -3,58 +3,47 @@ import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/User";
 
-const register = (req: Request, res: Response) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        ...req.body,
-        password: hash,
-      });
+const register = async (req: Request, res: Response) => {
+  try {
+    const hash = await bcrypt.hash(req.body.password, 10);
+    const user = new User({ ...req.body, password: hash });
 
-      user
-        .save()
-        .then(() =>
-          res.status(201).json({ message: "User registered successfully" })
-        )
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
 
-const login = (req: Request, res: Response) => {
-  User.findOne({ matricule: req.body.matricule })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ message: "User not found" });
-      }
+const login = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOne({ matricule: req.body.matricule });
+    if (!user) return res.status(401).json({ message: "User not found" });
 
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((valid) => {
-          if (!valid) {
-            return res.status(401).json({ message: "Wrong password" });
-          }
+    const valid = await bcrypt.compare(req.body.password, user.password);
+    if (!valid) return res.status(401).json({ message: "Wrong password" });
 
-          res.status(200).json({
-            userId: user._id,
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
-              expiresIn: "24h",
-            }),
-            name: user.name,
-            firstName: user.firstName,
-            matricule: user.matricule,
-          });
-        })
-        .catch((error) => res.status(500).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+    res.status(200).json({
+      userId: user._id,
+      token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+        expiresIn: "24h",
+      }),
+      name: user.name,
+      firstName: user.firstName,
+      matricule: user.matricule,
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
-const deleteUser = (req: Request, res: Response) => {
-  User.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "User deleted successfully" }))
-    .catch((error) => res.status(400).json({ error }));
+const deleteUser = async (req: Request, res: Response) => {
+  try {
+    await User.deleteOne({ _id: req.params.id });
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
 
 const UserCtrl = { register, login, deleteUser };
